bcf55b0019ff611eb2b1c839def3b03c04a07ec9
BagusNaren <wiwekanaren@gmail>
2025-06-25 12:41:17 +0800

    Added Main.java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import Tugas2.Server;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args.length == 1) {
+            port = Integer.parseInt(args[0]);
+        }
+        System.out.printf("Listening on port: %s...\n", port);
+        new Server(port);
+    }
+}
