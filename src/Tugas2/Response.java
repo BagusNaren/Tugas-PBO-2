86dded8abbc919c44826cf174319a89484ac4a42
BagusNaren <wiwekanaren@gmail>
2025-06-25 15:25:22 +0800

    Added Response.java

---
+++ src/Tugas2/Response.java
@@ -0,0 +1,51 @@
+package Tugas2;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class Response {
+
+    private final HttpExchange httpExchange;
+    private Headers headers;
+    private final StringBuilder stringBuilder;
+    private boolean isSent;
+
+    public Response(HttpExchange httpExchange) {
+        this.httpExchange = httpExchange;
+        this.stringBuilder = new StringBuilder();
+        this.isSent = false;
+    }
+
+    public void setBody(String string) {
+        stringBuilder.setLength(0);
+        stringBuilder.append(string);
+    }
+
+    public void send(int status) {
+        try {
+            this.httpExchange.getResponseHeaders().add("Content-Type", "application/json; charset=utf-8");
+
+            String body = stringBuilder.toString();
+            byte[] bodyBytes = body.getBytes(StandardCharsets.UTF_8);
+
+            this.httpExchange.sendResponseHeaders(status, bodyBytes.length);
+            this.httpExchange.getResponseBody().write(bodyBytes);
+            this.httpExchange.getResponseBody().flush();
+            this.httpExchange.getResponseBody().close(); // pastikan stream ditutup
+        } catch (IOException ioe) {
+            System.err.println("Problem encountered when sending response.");
+            ioe.printStackTrace();
+        }
+        this.isSent = true;
+    }
+
+
+    public boolean isSent() {
+        if (this.httpExchange.getResponseCode() != -1)
+            this.isSent = true;
+        return !isSent;
+    }
+}
